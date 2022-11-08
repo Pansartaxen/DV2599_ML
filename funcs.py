1e40d197b61b9dc49a24fb096709cecf3339f548
ziggokill <sebastianbengtsson@me.com>
2022-11-08 13:16:06 +0100

    Small fixes

--- funcs.py
+++ funcs.py
@@ -28,22 +28,39 @@ def get_diff(data: pd.DataFrame, data2: pd.DataFrame):
     words = [col for col in words1 if col not in words2]
     return words
 
+def normalized_to_values(dF : pd.DataFrame):
+    """Changes the normalized values to either low (0-0.33) or mid (0.33-0.66) or high (0.66-1)"""
+    for row in dF.index:
+        for col in dF.columns:
+            if dF.loc[row,col] < 0.33:
+                dF.loc[row,col] = 'low'
+            elif dF.loc[row,col] < 0.66:
+                dF.loc[row,col] = 'mid'
+            else:
+                dF.loc[row,col] = 'high'
+    return dF
+
 def discrete_transform(spam: pd.DataFrame, nonspam: pd.DataFrame):
     cols = spam.columns.values.tolist()
     discrete = pd.DataFrame(columns=cols)
     discrete.loc[0] = False
     for col in spam.columns:
-        if spam[col].mean() - nonspam[col].mean() > 0.1:
+        if spam[col] != nonspam[col] and spam[col] != "low":
             print(col)
             discrete[col] = True
     return discrete
 
 def normalize(dF : pd.DataFrame):
-    return df.iloc[:,0:-1].apply(lambda x: (x-x.min())/ (x.max() - x.min()), axis=0)
+    """Returns a normalized version of the input DataFrame"""
+    return df.iloc[:,0:-4].apply(lambda x: (x-x.min())/ (x.max() - x.min()), axis=1)
+    # axis is 1 because we want to normalize each row, not each column
 
 if __name__ == '__main__':
     df = get_data()
     spam = df.loc[df["spam"] == 1]
     nonspam = df.loc[df["spam"] == 0]
     norm_spam = normalize(spam)
-    
+    norm_nonspam = normalize(nonspam)
+    val_spam = normalized_to_values(norm_spam)
+    val_nonspam = normalized_to_values(norm_nonspam)
+    discrete = discrete_transform(val_spam, val_nonspam)
