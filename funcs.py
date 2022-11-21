d1f99586dd7f7279c4cdeb82aec7f39a4b10fdcf
ziggokill <sebastianbengtsson@me.com>
2022-11-21 15:19:51 +0100

    alert major bag (alert major bag)

--- funcs.py
+++ funcs.py
@@ -1,5 +1,10 @@
 import pandas as pd
 from matplotlib import pyplot as plt
+from sklearn import preprocessing
+from sklearn.model_selection import train_test_split
+import numpy as np
+from collections import Counter
+import random
 
 def get_data():
     df = pd.read_csv('spambase.csv')
@@ -7,52 +12,99 @@ def get_data():
 
 def lgg_conj(H : pd.DataFrame, x : pd.DataFrame):
     """Returns the least general generalization of H and x"""
-    return H.where(H == x, True)
+    y = H.copy()
+    for col in range(H.shape[0]):
+        a,b = H.iloc[col], x.iloc[col]
+        if H.iloc[col] == x.iloc[col] and H.iloc[col]:
+            y.iloc[col] = 1
+        else:
+            y.iloc[col] = 0
+    return y
 
 def lgg_set(data : pd.DataFrame):
-    row = data.iloc[0]
-    H = row
-    for row in data.iloc[1:]:
-        x = row
+    H = data.iloc[0]
+    for row in range(data.shape[0]):
+        x = data.iloc[row]
         H = lgg_conj(H, x)
     return H
 
-def normalized_to_values(dF : pd.DataFrame):
-    """Changes the normalized values to either low (0-0.33) or mid (0.33-0.66) or high (0.66-1)"""
-    for row in range(dF.shape[0]): # for each row
-        for col in range(dF.shape[1]): # for each column
-            if dF.iloc[row, col] < 0.5:
-                dF.iloc[row, col] = 0
+def continuous_to_discrete(data : pd.DataFrame):
+    """If value is above 0.01, set to 1, else 0"""
+    retDF = data.copy()
+    for col in data.columns:
+        retDF[col] = retDF[col].apply(lambda x: 1 if float(x) > 0.01 else 0)
+    return retDF
+
+def test_performance(data: pd.DataFrame, H: pd.Series):
+    """Returns the accuracy of the hypothesis"""
+    spam_as_spam = 0
+    spam_as_ham = 0
+    ham_as_ham = 0
+    ham_as_spam = 0
+
+    correct = 0
+
+    H_values = []
+    for keys in H.keys():
+        if H[keys] == True:
+            H_values.append(keys)
+    print(H_values)
+    for row in range(data.shape[0]):
+        spam = True
+        for col in range(data.shape[1]-1):
+            if H.iloc[col]:
+                if not data.iloc[row, col]:
+                    spam = False
+        if spam:
+            if data.iloc[row, -1]:
+                spam_as_spam += 1
+                correct += 1
             else:
-                dF.iloc[row,col] = 1
-    return dF
+                ham_as_spam += 1
+        else:
+            if data.iloc[row, -1]: 
+                spam_as_ham += 1
+            else:
+                ham_as_ham += 1
+                correct += 1
+        
+    print("good Spam as spam: ", spam_as_spam)
+    print("bad Spam as ham: ", spam_as_ham)
+    print("good Ham as ham: ", ham_as_ham)
+    print("bad Ham as spam: ", ham_as_spam)
+    print("Accuracy: ", correct/data.shape[0])
+    return correct/data.shape[0]
 
-def normalize(dF : pd.DataFrame):
-    """Returns a normalized version of the input DataFrame"""
-    return dF.iloc[:,0:-4].apply(lambda x: (x-x.min())/ (x.max() - x.min()), axis=1)
-    # axis is 1 because we want to normalize each row, not each column
-
-def make_lgg(dF : pd.DataFrame):
-    noramlized = normalize(dF)
-    to_values = normalized_to_values(noramlized)
-    return to_values
-    get_lgg = lgg_set(to_values)
-    return get_lgg
-
-def find_amount_of_zeros(dF : pd.DataFrame):
-    """Returns the amount of zeros in the dataframe"""
-    return dF[dF == 0].count().sum()
+def most_above_zero(dF : pd.DataFrame):
+    """Returns the columns with the percentage of values above 0 and sorted by the percentage"""
+    above_zero = {}
+    for col in dF.columns:
+        above_zero[col] = dF[dF[col] > 0].count()[col]/dF.shape[0]
+    return sorted(above_zero.items(), key=lambda x: x[1], reverse=True)
 
 if __name__ == '__main__':
     print('På grinden!')
     df = get_data()
-    print(find_amount_of_zeros(df))
 
-    spam = df.loc[df["spam"] == 1].copy(deep=True)
-    spam_lgg = make_lgg(spam)
-    print(spam_lgg.head())
-    print("\n\n\n")
+    spam = df[df['spam'] == 1].copy()
+    ham = df[df['spam'] == 0].copy()
+    
+    initiated = True
 
-    nonspam = df.loc[df["spam"] == 0].copy(deep=True)
-    nonspam_lgg = make_lgg(nonspam)
-    print(nonspam_lgg.head())
+    while initiated or len(h[h==1]) < 5:
+        initiated = False
+        train_data, split_data = train_test_split(spam, test_size=0.997)
+
+        train_data = continuous_to_discrete(train_data)
+        split_data = continuous_to_discrete(split_data)
+
+        h = lgg_set(train_data)
+
+    old_joined = [ham, split_data]
+    joined = pd.concat(old_joined)
+
+    print(test_performance(joined, h))
+
+    print("Bra pluggat idag grabbar!")
+
+    
