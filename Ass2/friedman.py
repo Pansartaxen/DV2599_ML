aca6f897be20ea634b5e9d6d27edf433c662e8bf
Marius Stokkedal <matk20@student.bth.se>
2022-12-06 16:14:29 +0100

    Added doc strings and friedman done

--- Ass2/friedman.py
+++ Ass2/friedman.py
@@ -1,4 +1,8 @@
 def row_ranker(row: list) -> list:
+    """
+    Takes a list as input and returns a list with
+    the ranks of the values where 1 is lowest
+    """
     score = 1
     score_dict = {}
     for val in set(row):
@@ -10,10 +14,11 @@ def row_ranker(row: list) -> list:
         score_dict[val] = tot/count
     return [score_dict[val] for val in row]
 
-def average(lst: list):
+def average(lst: list) -> float:
+    """Takes in a list and returns the average value"""
     return sum(lst)/len(lst)
 
-def friedman(knn: list, svm: list, DT: list):
+def friedman(knn: list, svm: list, DT: list) -> float:
     """Returns the friedman test statistic"""
 
     for i in range(len(knn)):
