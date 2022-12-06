188e6cdcfdfbc4f2e075a13206d2a7e54288fed5
Marius Stokkedal <matk20@student.bth.se>
2022-12-06 13:51:02 +0100

    Changed whole friedman

--- Ass2/friedman.py
+++ Ass2/friedman.py
@@ -2,16 +2,28 @@ import pandas as pd
 
 def get_data():
     """Returns the data as a pandas dataframe"""
-    df = pd.read_csv('Ass2\spambase.csv')
+    df = pd.read_csv('/Users/mariusstokkedal/Desktop/DV2599_Maskininlärning/DV2599_ML/Ass2/spambase.csv')
     return df
 
-def friedman(data: pd.DataFrame):
-    column_sum = 0
-    for column in range(data.shape[1]):
-        column_sum += (data.iloc[:,column].sum)**2
+def row_ranker(row: list) -> list:
+    score = 1
+    score_dict = {}
+    for val in set(row):
+        count = row.count(val)
+        tot = 0
+        for i in range(count):
+            tot += i+score
+        score += count
+        score_dict[val] = tot/count
+    return [score_dict[val] for val in row]
 
-    cols = data.shape[1]
-    rows = data.shape[0]
-    return (12/(rows*cols*(cols+1))) * column_sum - 3*rows*(cols+1)
+def average(lst: list):
+    return sum(lst)/len(lst)
+
+def friedman(knn: list, svm: list, DT: list):
+    avg_rank = (average(knn) + average(svm) + average(DT)) / 3
+    return 10 * ((average(knn) - avg_rank) ** 2 + (average(svm) - avg_rank) ** 2 + (average(DT) - avg_rank) ** 2)
+
 
 data = get_data()
+print(friedman(data))
