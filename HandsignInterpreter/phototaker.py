9fbbba375f81fad3bcb1ebbca08f9e031d92c326
ziggokill <sebastianbengtsson@me.com>
2022-12-12 14:45:35 +0100

    Run camera

--- HandsignInterpreter/phototaker.py
+++ HandsignInterpreter/phototaker.py
@@ -2,42 +2,71 @@ import cv2
 from PIL import Image
 from numpy import asarray
 
-
-# Open the camera
-cap = cv2.VideoCapture(0)
-
-capturing = True
-
-# Capture frames from the camera
-while capturing:
-    # Read the next frame from the camera
-    ret, frame = cap.read()
-
-    # Check if the frame was successfully read
-    if ret:
-        # Display the frame
-        cv2.imshow('Camera', frame)
-
-        # Check if the user pressed the 'q' key
-
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            capturing = False
-
-        # Check if the user pressed the 'SPACE' key
-        if cv2.waitKey(1) & 0xFF == ord(' '):
-            # Save the image
-            cv2.imwrite('HandsignInterpreter\latest.png', frame)
-
-# Release the camera
-cap.release()
-cv2.destroyAllWindows()
-
 def png_to_array(path):
     png = Image.open(path)
     png_array = asarray(png)
 
-    print(png_array[0][0])
-    print(png_array.shape)
+    return png_array
+
+
+def classify_image():
+    letter = ""
+    image_array = png_to_array('HandsignInterpreter\latest.png')
+
+    if letter == "":
+        letter = "?"
+    
+    return letter
+
+def run_camera():
+    # Open the camera
+    cap = cv2.VideoCapture(0)
+
+    # Set the camera resolution
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+    info = """Press space to read a new letter,"""
+    info2 = """c to clear text, q to quit"""
+    text = ""
+
+    # Loop until the user hits the 'q' key
+    while True:
+        # Read a frame from the camera
+        ret, frame = cap.read()
+
+        # Check if the frame was successfully read
+        if not ret:
+            break
+
+        # Wait for the user to hit a key
+        key = cv2.waitKey(1)
+        if key == ord('q'):
+            break
+    
+        if key == ord('c'):
+            text = ""
+        
+
+
+        # If the user pressed 'L', put some text on the frame
+        if key == ord(' '):
+            new_letter = classify_image()
+            text += new_letter
+            print("Found letter",new_letter)
+        
+        cv2.putText(frame, info, (20, 460), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (100, 200, 100), 2)
+        cv2.putText(frame, info2, (20, 440), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (100, 200, 100), 2)
+        cv2.putText(frame, text, (20, 50), cv2.FONT_HERSHEY_SIMPLEX, 2.0, (0, 0, 0), 2)
+        cv2.rectangle(frame, (50, 80), (246, 276), (0, 255, 0), 2)
+
+
+        # Show the frame
+        cv2.imshow("Camera", frame)
+
+    # Release the camera and destroy all windows
+    cap.release()
+    cv2.destroyAllWindows()
 
 if __name__ == "__main__":
-    png_to_array('HandsignInterpreter/test.png')
+    #png_to_array('HandsignInterpreter/test.png')
+    run_camera()
