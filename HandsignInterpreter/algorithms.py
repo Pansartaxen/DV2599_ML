4b2d21d84e8d9158f79f32a8002e75a516052157
Marius Stokkedal <matk20@student.bth.se>
2022-12-14 15:27:54 +0100

    Added CNN

--- HandsignInterpreter/algorithms.py
+++ HandsignInterpreter/algorithms.py
@@ -5,7 +5,14 @@ import pickle
 
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.svm import SVC
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import StandardScaler, LabelBinarizer
+
+import keras
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Sequential
+from sklearn.metrics import classification_report,confusion_matrix
+from keras.callbacks import ReduceLROnPlateau
+from keras.layers import Dense, Conv2D , MaxPool2D , Flatten , Dropout , BatchNormalization
 
 
 letters = {
@@ -122,6 +129,63 @@ def train_svm():
     pickle.dump(svm, open('HandsignInterpreter/finalized_model_svm.sav', 'wb'))
     return sc
 
+def cnn_train():
+    df = get_data()
+    df_test = get_test_data()
+    classes_train = df['label']
+    classes_test = df_test['label']
+    del df['label']
+    del df_test['label']
+
+    label_binarizer = LabelBinarizer()
+    classes_train = label_binarizer.fit_transform(classes_train)
+    classes_test = label_binarizer.fit_transform(classes_test)
+
+    vector_train = df.values
+    vector_test = df_test.values
+    vector_train = vector_train / 255
+    vector_test = vector_test / 255
+    vector_train = vector_train.reshape(-1, 28, 28, 1)
+    vector_test = vector_test.reshape(-1, 28, 28, 1)
+
+    datagen = ImageDataGenerator(
+        featurewise_center=False,  # set input mean to 0 over the dataset
+        samplewise_center=False,  # set each sample mean to 0
+        featurewise_std_normalization=False,  # divide inputs by std of the dataset
+        samplewise_std_normalization=False,  # divide each input by its std
+        zca_whitening=False,  # apply ZCA whitening
+        rotation_range=10,  # randomly rotate images in the range (degrees, 0 to 180)
+        zoom_range = 0.1, # Randomly zoom image
+        width_shift_range=0.1,  # randomly shift images horizontally (fraction of total width)
+        height_shift_range=0.1,  # randomly shift images vertically (fraction of total height)
+        horizontal_flip=False,  # randomly flip images
+        vertical_flip=False)  # randomly flip images
+
+    datagen.fit(vector_train)
+
+    learning_rate_reduction = ReduceLROnPlateau(monitor='val_accuracy', patience = 2, verbose=1,factor=0.5, min_lr=0.00001)
+
+    model = Sequential()
+    model.add(Conv2D(75 , (3,3) , strides = 1 , padding = 'same' , activation = 'relu' , input_shape = (28,28,1)))
+    model.add(BatchNormalization())
+    model.add(MaxPool2D((2,2) , strides = 2 , padding = 'same'))
+    model.add(Conv2D(50 , (3,3) , strides = 1 , padding = 'same' , activation = 'relu'))
+    model.add(Dropout(0.2))
+    model.add(BatchNormalization())
+    model.add(MaxPool2D((2,2) , strides = 2 , padding = 'same'))
+    model.add(Conv2D(25 , (3,3) , strides = 1 , padding = 'same' , activation = 'relu'))
+    model.add(BatchNormalization())
+    model.add(MaxPool2D((2,2) , strides = 2 , padding = 'same'))
+    model.add(Flatten())
+    model.add(Dense(units = 512 , activation = 'relu'))
+    model.add(Dropout(0.3))
+    model.add(Dense(units = 24 , activation = 'softmax'))
+    model.compile(optimizer = 'adam' , loss = 'categorical_crossentropy' , metrics = ['accuracy'])
+    model.summary()
+
+    history = model.fit(datagen.flow(vector_train, classes_train, batch_size = 128), epochs = 20, validation_data = (vector_test, classes_test), callbacks = [learning_rate_reduction])
+    print("Accuracy of the model is - " , model.evaluate(vector_test, classes_test)[1]*100 , "%")
+
 def classify_image_RF(image, clf):
     """Returns the letter that the image is classified as"""
     letter = clf.predict(image)
@@ -137,4 +201,4 @@ def classify_image_svm(image, svm, sc):
 
 if __name__ == "__main__":
     print("Running algorithms.py as main")
-    rm_acc, random_forest = train_random_forest()
+    cnn_train()
--- HandsignInterpreter/create_df.py
+++ HandsignInterpreter/create_df.py
@@ -6,5 +6,6 @@ def convert_image(path, label, dataframe):
     img = plt.imread(path)
     row = np.array(img).flatten()
     row = np.append(row, label)
+    index = dataframe.shape[0]
     dataframe.loc[len(dataframe)] = row
     return dataframe
